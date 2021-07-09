01a886a89145abe71fe9693ac43f0163e5bb80ca
JaeHeee <kjha2142@gmail.com>
2021-07-10 02:55:33 +0900

    feat: Add home page

---
+++ lib/constants/data.dart
@@ -0,0 +1,51 @@
+const items = [
+  {
+    'title': 'Face Detection',
+    'text':
+        'MediaPipe Face Detection is an ultrafast face detection solution that comes with 6 landmarks and multi-face support. It is based on BlazeFace, a lightweight and well-performing face detector tailored for mobile GPU inference.',
+    'image': 'assets/images/architecture.jpg',
+    'model': 'models/face_detection_full_range_sparse.tflite',
+  },
+  {
+    'title': 'Face Mesh',
+    'text':
+        'MediaPipe Face Mesh is a face geometry solution that estimates 468 3D face landmarks in real-time even on mobile devices. It employs machine learning (ML) to infer the 3D surface geometry, requiring only a single camera input without the need for a dedicated depth sensor.',
+    'image': 'assets/images/street_musician.jpg',
+    'model': 'models/face_landmark.tflite',
+  },
+  {
+    'title': 'Iris',
+    'text':
+        'A wide range of real-world applications, including computational photography (glint reflection) and augmented reality effects (virtual avatars) rely on accurately tracking the iris within an eye.',
+    'image': 'assets/images/man.jpg',
+    'model': 'models/iris_landmark.tflite',
+  },
+  {
+    'title': 'Hands',
+    'text':
+        'The ability to perceive the shape and motion of hands can be a vital component in improving the user experience across a variety of technological domains and platforms.',
+    'image': 'assets/images/new_york_city.jpg',
+    'model': 'models/hand_landmark_sparse.tflite',
+  },
+  {
+    'title': 'Holistic',
+    'text':
+        'Live perception of simultaneous human pose, face landmarks, and hand tracking in real-time on mobile devices can enable various modern life applications: fitness and sport analysis, gesture control and sign language recognition, augmented reality try-on and effects.',
+    'image': 'assets/images/paris.jpg',
+    'model': 'models/hand_recrop.tflite',
+  },
+  {
+    'title': 'Pose Detection',
+    'text':
+        'Human pose estimation from video plays a critical role in various applications such as quantifying physical exercises, sign language recognition, and full-body gesture control.',
+    'image': 'assets/images/st_petersburg_russia.jpg',
+    'model': 'models/pose_detection.tflite',
+  },
+  {
+    'title': 'Pose Landmark',
+    'text':
+        'Human pose estimation from video plays a critical role in various applications such as quantifying physical exercises, sign language recognition, and full-body gesture control.',
+    'image': 'assets/images/train.jpg',
+    'model': 'models/pose_landmark.tflite',
+  },
+];
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,154 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../constants/data.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController _pageController;
+  double _currentPageValue = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(viewportFraction: 0.8)
+      ..addListener(() {
+        setState(() {
+          _currentPageValue = _pageController.page;
+        });
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: Text(
+        'Select Your Model',
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: ScreenUtil().setSp(28),
+            fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Stack _buildBody() {
+    return Stack(
+      children: [
+        _buildBackgroundImage(),
+        _buildModelPageView(),
+      ],
+    );
+  }
+
+  Center _buildModelPageView() {
+    return Center(
+      child: Container(
+        height: ScreenUtil().setHeight(450.0),
+        child: PageView.builder(
+          controller: _pageController,
+          physics: const BouncingScrollPhysics(),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            var scale = (_currentPageValue - index).abs();
+            return _buildModelCard(index, scale);
+          },
+        ),
+      ),
+    );
+  }
+
+  Container _buildModelCard(int index, double scale) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        image: DecorationImage(
+          image: AssetImage(items[index]['image']),
+          fit: BoxFit.cover,
+        ),
+      ),
+      margin: EdgeInsets.symmetric(
+        horizontal: ScreenUtil().setWidth(10.0),
+        vertical: ScreenUtil().setHeight(30.0) * scale,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.2),
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(15.0),
+                bottomRight: Radius.circular(15.0),
+              ),
+            ),
+            padding: EdgeInsets.all(ScreenUtil().setWidth(16.0)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  items[index]['title'],
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: ScreenUtil().setSp(20.0),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: ScreenUtil().setHeight(8.0),
+                ),
+                Text(
+                  items[index]['text'],
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: ScreenUtil().setSp(12.0),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildBackgroundImage() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            items[_currentPageValue.round()]['image'],
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: 5.0,
+          sigmaY: 5.0,
+        ),
+        child: Container(
+          color: Colors.black.withOpacity(0.15),
+        ),
+      ),
+    );
+  }
+}
