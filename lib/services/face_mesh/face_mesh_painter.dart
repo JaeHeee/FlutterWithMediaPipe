0e7d59712a41fefce377fb6389b7d1960535a5c8
JaeHeee <kjha2142@gmail.com>
2021-08-28 23:40:02 +0900

    feat: Add faceMesh service

---
+++ lib/services/face_mesh/face_mesh_painter.dart
@@ -0,0 +1,26 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class FaceMeshPainter extends CustomPainter {
+  final List<Offset> points;
+  final double ratio;
+
+  FaceMeshPainter({
+    @required this.points,
+    @required this.ratio,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint1 = Paint()
+      ..color = Colors.amber
+      ..strokeWidth = 4;
+
+    canvas.drawPoints(PointMode.points,
+        points.map((point) => point * ratio).toList(), paint1);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
---
+++ lib/services/face_mesh/face_mesh_service.dart
@@ -0,0 +1,96 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image/image.dart' as image_lib;
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
+
+class FaceMesh {
+  static const String MODEL_FILE_NAME = 'models/face_landmark.tflite';
+  static const int INPUT_SIZE = 192;
+
+  InterpreterOptions _interpreterOptions;
+  Interpreter _interpreter;
+  ImageProcessor _imageProcessor;
+
+  List<List<int>> _outputShapes;
+  List<TfLiteType> _outputTypes;
+
+  Interpreter get interpreter => _interpreter;
+
+  FaceMesh({Interpreter interpreter}) {
+    _loadModel(interpreter: interpreter);
+  }
+
+  void _loadModel({Interpreter interpreter}) async {
+    try {
+      _interpreterOptions = InterpreterOptions();
+
+      _interpreter = interpreter ??
+          await Interpreter.fromAsset(MODEL_FILE_NAME,
+              options: _interpreterOptions);
+
+      var outputTensors = _interpreter.getOutputTensors();
+      _outputShapes = [];
+      _outputTypes = [];
+      outputTensors.forEach((tensor) {
+        _outputShapes.add(tensor.shape);
+        _outputTypes.add(tensor.type);
+      });
+    } catch (e) {
+      print('Error while creating interpreter: $e');
+    }
+  }
+
+  TensorImage _getProcessedImage(TensorImage inputImage) {
+    _imageProcessor ??= ImageProcessorBuilder()
+        .add(ResizeOp(INPUT_SIZE, INPUT_SIZE, ResizeMethod.BILINEAR))
+        .add(NormalizeOp(0, 255))
+        .build();
+
+    inputImage = _imageProcessor.process(inputImage);
+    return inputImage;
+  }
+
+  Map<String, dynamic> predict(image_lib.Image image) {
+    if (_interpreter == null) {
+      print('Interpreter not initialized');
+      return null;
+    }
+
+    if (Platform.isAndroid) {
+      image = image_lib.copyRotate(image, -90);
+      image = image_lib.flipHorizontal(image);
+    }
+    var tensorImage = TensorImage(TfLiteType.float32);
+    tensorImage.loadImage(image);
+    var inputImage = _getProcessedImage(tensorImage);
+
+    TensorBuffer outputLandmarks = TensorBufferFloat(_outputShapes[0]);
+    TensorBuffer outputScores = TensorBufferFloat(_outputShapes[1]);
+
+    var inputs = <Object>[inputImage.buffer];
+
+    var outputs = <int, Object>{
+      0: outputLandmarks.buffer,
+      1: outputScores.buffer,
+    };
+
+    _interpreter.runForMultipleInputs(inputs, outputs);
+
+    if (outputScores.getDoubleValue(0) < 0) {
+      return null;
+    }
+
+    var landmarkPoints = outputLandmarks.getDoubleList().reshape([468, 3]);
+    var landmarkResults = <Offset>[];
+    for (var point in landmarkPoints) {
+      landmarkResults.add(Offset(
+        point[0] / INPUT_SIZE * image.width,
+        point[1] / INPUT_SIZE * image.height,
+      ));
+    }
+
+    return {'point': landmarkResults};
+  }
+}
