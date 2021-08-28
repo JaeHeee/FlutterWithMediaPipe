b9ac84302c3c6934f62c874935c39882f4200b22
JaeHeee <kjha2142@gmail.com>
2021-08-29 03:28:00 +0900

    feat: Add Pose service

---
+++ lib/services/pose/pose_painter.dart
@@ -0,0 +1,143 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class PosePainter extends CustomPainter {
+  final List<Offset> points;
+  final double ratio;
+
+  PosePainter({
+    @required this.points,
+    @required this.ratio,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var pointPaint = Paint()
+      ..color = Colors.black
+      ..strokeWidth = 8;
+    var headPaint = Paint()
+      ..color = Colors.deepOrange
+      ..strokeWidth = 2;
+    var leftPaint = Paint()
+      ..color = Colors.lightBlue
+      ..strokeWidth = 2;
+    var rightPaint = Paint()
+      ..color = Colors.yellow
+      ..strokeWidth = 2;
+    var bodyPaint = Paint()
+      ..color = Colors.pink
+      ..strokeWidth = 2;
+
+    canvas.drawPoints(
+      PointMode.points,
+      points.sublist(0, 34).map((point) => point * ratio).toList(),
+      pointPaint,
+    );
+
+    canvas.drawPoints(
+      PointMode.polygon,
+      [
+        points[8],
+        points[6],
+        points[5],
+        points[4],
+        points[0],
+        points[1],
+        points[2],
+        points[3],
+        points[7],
+      ].map((point) => point * ratio).toList(),
+      headPaint,
+    );
+    canvas.drawPoints(
+      PointMode.polygon,
+      [
+        points[10],
+        points[9],
+      ].map((point) => point * ratio).toList(),
+      headPaint,
+    );
+
+    canvas.drawPoints(
+      PointMode.polygon,
+      [
+        points[12],
+        points[14],
+        points[16],
+        points[18],
+        points[20],
+        points[16],
+      ].map((point) => point * ratio).toList(),
+      leftPaint,
+    );
+    canvas.drawPoints(
+      PointMode.polygon,
+      [
+        points[16],
+        points[22],
+      ].map((point) => point * ratio).toList(),
+      leftPaint,
+    );
+    canvas.drawPoints(
+      PointMode.polygon,
+      [
+        points[24],
+        points[26],
+        points[28],
+        points[32],
+        points[30],
+        points[28],
+      ].map((point) => point * ratio).toList(),
+      leftPaint,
+    );
+
+    canvas.drawPoints(
+      PointMode.polygon,
+      [
+        points[11],
+        points[13],
+        points[15],
+        points[17],
+        points[19],
+        points[15],
+      ].map((point) => point * ratio).toList(),
+      rightPaint,
+    );
+    canvas.drawPoints(
+      PointMode.polygon,
+      [
+        points[15],
+        points[21],
+      ].map((point) => point * ratio).toList(),
+      rightPaint,
+    );
+    canvas.drawPoints(
+      PointMode.polygon,
+      [
+        points[23],
+        points[25],
+        points[27],
+        points[29],
+        points[31],
+        points[27],
+      ].map((point) => point * ratio).toList(),
+      rightPaint,
+    );
+
+    canvas.drawPoints(
+      PointMode.polygon,
+      [
+        points[11],
+        points[12],
+        points[24],
+        points[23],
+        points[11],
+      ].map((point) => point * ratio).toList(),
+      bodyPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
---
+++ lib/services/pose/pose_service.dart
@@ -0,0 +1,104 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image/image.dart' as image_lib;
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
+
+class Pose {
+  static const String MODEL_FILE_NAME = 'models/pose_landmark_full.tflite';
+  static const int INPUT_SIZE = 256;
+  static const double THRESHOLD = 0.8;
+
+  InterpreterOptions _interpreterOptions;
+  Interpreter _interpreter;
+  ImageProcessor _imageProcessor;
+
+  List<List<int>> _outputShapes;
+  List<TfLiteType> _outputTypes;
+
+  Interpreter get interpreter => _interpreter;
+
+  Pose({Interpreter interpreter}) {
+    _loadModel(interpreter: interpreter);
+  }
+
+  void _loadModel({Interpreter interpreter}) async {
+    try {
+      _interpreterOptions = InterpreterOptions();
+
+      _interpreter = interpreter ??
+          await Interpreter.fromAsset(MODEL_FILE_NAME,
+              options: _interpreterOptions);
+
+      var outputTensors = _interpreter.getOutputTensors();
+      _outputShapes = [];
+      _outputTypes = [];
+      outputTensors.forEach((tensor) {
+        _outputShapes.add(tensor.shape);
+        _outputTypes.add(tensor.type);
+      });
+    } catch (e) {
+      print('Error while creating interpreter: $e');
+    }
+  }
+
+  TensorImage _getProcessedImage(TensorImage inputImage) {
+    _imageProcessor ??= ImageProcessorBuilder()
+        .add(ResizeOp(INPUT_SIZE, INPUT_SIZE, ResizeMethod.BILINEAR))
+        .add(NormalizeOp(0, 255))
+        .build();
+
+    inputImage = _imageProcessor.process(inputImage);
+    return inputImage;
+  }
+
+  Map<String, dynamic> predict(image_lib.Image image) {
+    if (_interpreter == null) {
+      print('Interpreter not initialized');
+      return null;
+    }
+
+    if (Platform.isAndroid) {
+      image = image_lib.copyRotate(image, -90);
+      image = image_lib.flipHorizontal(image);
+    }
+    var tensorImage = TensorImage(TfLiteType.float32);
+    tensorImage.loadImage(image);
+    var inputImage = _getProcessedImage(tensorImage);
+
+    TensorBuffer outputLandmarks = TensorBufferFloat(_outputShapes[0]);
+    TensorBuffer outputIdentity1 = TensorBufferFloat(_outputShapes[1]);
+    TensorBuffer outputIdentity2 = TensorBufferFloat(_outputShapes[2]);
+    TensorBuffer outputIdentity3 = TensorBufferFloat(_outputShapes[3]);
+    TensorBuffer outputIdentity4 = TensorBufferFloat(_outputShapes[4]);
+
+    var inputs = <Object>[inputImage.buffer];
+
+    var outputs = <int, Object>{
+      0: outputLandmarks.buffer,
+      1: outputIdentity1.buffer,
+      2: outputIdentity2.buffer,
+      3: outputIdentity3.buffer,
+      4: outputIdentity4.buffer,
+    };
+
+    _interpreter.runForMultipleInputs(inputs, outputs);
+
+    if (outputIdentity1.getDoubleValue(0) < THRESHOLD) {
+      return null;
+    }
+
+    var landmarkPoints = outputLandmarks.getDoubleList().reshape([39, 5]);
+    var landmarkResults = <Offset>[];
+
+    for (var point in landmarkPoints) {
+      landmarkResults.add(Offset(
+        point[0] / INPUT_SIZE * image.width,
+        point[1] / INPUT_SIZE * image.height,
+      ));
+    }
+
+    return {'point': landmarkResults};
+  }
+}
