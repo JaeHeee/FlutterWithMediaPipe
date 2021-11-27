1e447874c41c27677a7ecb6783df3d1338631dc5
JaeHeee <kjha2142@gmail.com>
2021-11-27 20:30:20 +0900

    feat: Add ModelInferenceService

---
+++ lib/services/model_inference_service.dart
@@ -0,0 +1,64 @@
+import 'dart:isolate';
+
+import 'package:camera/camera.dart';
+
+import '../utils/isolate_utils.dart';
+import 'face_detection/face_detection_service.dart';
+import 'face_mesh/face_mesh_service.dart';
+import 'hands/hands_service.dart';
+import 'pose/pose_service.dart';
+import 'service_locator.dart';
+
+enum Models {
+  FaceDetection,
+  FaceMesh,
+  Hands,
+  Pose,
+}
+
+class ModelInferenceService {
+  late dynamic model;
+  late Function handler;
+  Map<String, dynamic>? inferenceResults;
+
+  Future<Map<String, dynamic>?> inference({
+    required IsolateUtils isolateUtils,
+    required CameraImage cameraImage,
+  }) async {
+    final responsePort = ReceivePort();
+
+    isolateUtils.sendMessage(
+      handler: handler,
+      params: {
+        'cameraImage': cameraImage,
+        'detectorAddress': model.getAddress,
+      },
+      sendPort: isolateUtils.sendPort,
+      responsePort: responsePort,
+    );
+
+    inferenceResults = await responsePort.first;
+    responsePort.close();
+  }
+
+  void setModelConfig(int index) {
+    switch (Models.values[index]) {
+      case Models.FaceDetection:
+        model = locator<FaceDetection>();
+        handler = runFaceDetector;
+        break;
+      case Models.FaceMesh:
+        model = locator<FaceMesh>();
+        handler = runFaceMesh;
+        break;
+      case Models.Hands:
+        model = locator<Hands>();
+        handler = runHandDetector;
+        break;
+      case Models.Pose:
+        model = locator<Pose>();
+        handler = runPoseEstimator;
+        break;
+    }
+  }
+}
