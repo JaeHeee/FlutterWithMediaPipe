af25da809c50e191fe700c4b3e658dd1acd537e7
JaeHeee <kjha2142@gmail.com>
2021-11-28 20:36:40 +0900

    feat: Add Model File mixin

---
+++ lib/constants/model_file.dart
@@ -0,0 +1,7 @@
+mixin ModelFile {
+  static const String faceDetection =
+      'models/face_detection_short_range.tflite';
+  static const String faceMesh = 'models/face_landmark.tflite';
+  static const String hands = 'models/hand_landmark.tflite';
+  static const String pose = 'models/pose_landmark_full.tflite';
+}
